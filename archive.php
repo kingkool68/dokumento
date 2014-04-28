3397a3fd4393abb3946a8676701022906fa22426
Russell Heimlich <kingkool68@gmail.com>
2014-04-28 18:21:34 -0400

    Added an archive.php template for tag and category archive views.

--- archive.php
+++ archive.php
@@ -1,8 +1,52 @@
-<?php get_header(); ?>
+<?php
+//Pretty much identicla to the edit_term_link() function in core except I wanted to add a class to the HTML. 
+function dokumento_edit_term_link( $link = '', $before = '', $after = '', $echo = true ) {
+	$term = get_queried_object();
 
+	if ( ! $term ) {
+		return;
+	}
+
+	$tax = get_taxonomy( $term->taxonomy );
+	if ( ! current_user_can( $tax->cap->edit_terms ) ) {
+		return;
+	}
+
+	if ( empty( $link ) ) {
+		$link = 'Edit';
+	}
+
+	$link = '<a href="' . get_edit_term_link( $term->term_id, $term->taxonomy ) . '" class="edit">' . $link . '</a>';
+
+	/**
+	 * Filter the anchor tag for the edit link of a term.
+	 *
+	 * @since 3.1.0
+	 *
+	 * @param string $link    The anchor tag for the edit link.
+	 * @param int    $term_id Term ID.
+	 */
+	$link = $before . apply_filters( 'edit_term_link', $link, $term->term_id ) . $after;
+
+	if ( $echo )
+		echo $link;
+	else
+		return $link;
+}
+
+get_header();
+?>
+
+<h1 class="page-title"><?php single_term_title();?> <?php dokumento_edit_term_link('Edit'); ?></h1>
 <div id="content" role="main">
+	<?php if( $term_description = term_description() ) : ?>
+		<div class="description">
+			<?php echo $term_description; ?>
+		</div>
+	<?php endif; ?>
+	
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<?php endwhile; else: ?>
 	
 	<?php endif; ?>
