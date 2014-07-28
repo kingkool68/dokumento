2665f4f2e45ad44896dd7a87649383e69181a19e
Russell Heimlich <kingkool68@gmail.com>
2014-07-28 10:26:07 -0400

    Made the Front Page an editable page. Revisions work for it as well.

--- front-page.php
+++ front-page.php
@@ -1,16 +1,1 @@
-<?php get_header(); ?>
-
-<div id="content" role="main">
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<?php the_content(); ?>
-		<div class="meta">
-			<p><span title="<?php esc_attr_e( date( get_option('date_format') . ' ' . get_option('time_format'), strtotime($post->post_modified)) ) ?>">Updated <?php echo dokumento_human_time_diff( 2, strtotime($post->post_modified) ); ?> ago</span> by <?php the_modified_author(); ?>.</p>
-		</div>
-	<?php endwhile; else: ?>
-	
-	<?php endif; ?>
-</div>
-
-<?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
+<?php get_template_part('single');
