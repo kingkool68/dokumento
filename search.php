02cbf0fe09a0868b602963339177b255e2fb9988
Russell Heimlich <kingkool68@gmail.com>
2014-08-06 17:50:30 -0400

    Option to create a new page from the search results. Meh...

--- search.php
+++ search.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 
 <div id="content" role="main">
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : ?>
+	<p>Create a new page called <a href="<?php echo get_admin_url()?>post-new.php?post_title=<?php echo urlencode( ucfirst( get_query_var('s') ) );?>"><?php echo ucfirst( get_search_query() ); ?></a>?</p>
+<?php while (have_posts()) : the_post(); ?>
 		<h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 		
 			<?php the_excerpt(); ?>
@@ -11,7 +13,7 @@
 			</div>
 	<?php endwhile; else: ?>
 		<h1 class="page-title">No results found!</h1>
-		<p>Create a new page called <a href="<?php echo get_admin_url()?>post-new.php?post_title=<?php echo urlencode( ucfirst( get_query_var('s') ) );?>"><?php echo ucfirst( get_search_query() ); ?></a>.</p>
+		<p>Create a new page called <a href="<?php echo get_admin_url()?>post-new.php?post_title=<?php echo urlencode( ucfirst( get_query_var('s') ) );?>"><?php echo ucfirst( get_search_query() ); ?></a>?</p>
 	<?php endif; ?>
 </div>
 <?php get_sidebar(); ?>
