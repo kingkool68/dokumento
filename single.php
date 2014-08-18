3b411c2ca564834091474bf4eaef40f554a8730a
Russell Heimlich <kingkool68@gmail.com>
2014-08-18 17:29:37 -0400

    Instead of Edit text, we can use a pencil icon.

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	
 		<?php if( !is_front_page() ): ?>
-			<h1 class="page-title"><?php the_title(); ?> <a href="<?php echo get_edit_post_link();?>" class="edit">Edit</a></h1> 
+			<h1 class="page-title"><?php the_title(); ?> <a href="<?php echo get_edit_post_link();?>" class="edit"><i class="icon-pencil"></i><span class="hidden">Edit</span></a></h1> 
 		<?php endif; ?>
 		<p class="freshness"><a href="<?php echo get_permalink( $post->ID ); ?>revisions/" title="<?php esc_attr_e( date( get_option('date_format') . ' ' . get_option('time_format'), strtotime($post->post_modified)) ) ?>" class="updated">Updated <?php echo dokumento_human_time_diff( 1, strtotime($post->post_modified) ); ?> ago</a> by <a href="" class="author"><?php the_modified_author(); ?></a></p>
 					
