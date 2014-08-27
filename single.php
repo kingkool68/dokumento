ad4f535a0beb5795ad1491570e10615e73997b14
Russell Heimlich <kingkool68@gmail.com>
2014-08-27 16:56:39 -0400

    No more Redirected From business.

--- single.php
+++ single.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post();
+	if( isset($_GET['from']) ) {
+		echo dokumento_create_new_page($_GET['from'] );
+	}
+?>
 
 			<h1 class="page-title"><?php the_title(); ?> <a href="<?php echo dokumento_get_edit_post_link();?>" class="edit" title="Edit"><i class="icon-pencil"></i><span class="hidden">Edit</span></a></h1> 
 		<p class="freshness"><a href="<?php echo get_permalink( $post->ID ); ?>revisions/" title="<?php esc_attr_e( date( get_option('date_format') . ' ' . get_option('time_format'), strtotime($post->post_modified)) ) ?>" class="updated">Updated <?php echo dokumento_human_time_diff( 1, strtotime($post->post_modified) ); ?> ago</a> by <a href="" class="author"><?php the_modified_author(); ?></a></p>
