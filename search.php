12ff920a0103df47c38277f764da5d6123f8b73a
Russell Heimlich <kingkool68@gmail.com>
2014-08-27 16:58:08 -0400

    Tweaked some things in search to give better results.
    Direct match is determined by doing a sql query instead of using WordPress' get_page_by_title() function. This is more of what we want.
    
    If your search is a direct match with a page title and there is only one result then we will redirect you right there. Otherwise we show the search results. This seems to be the right balance for now.

--- search.php
+++ search.php
@@ -1,26 +1,39 @@
 <?php
 global $wp_query;
-if( $wp_query->found_posts === 1 ) {
-	$post = $wp_query->posts[0];
-	$redirect_to = get_permalink( $post->ID );
-	if( get_permalink( $post->ID ) ) {
-		wp_redirect( $redirect_to );
+
+function is_direct_match() {
+	global $wpdb;
+	
+	$args = array(
+		'exclude_from_search' => false
+	);
+	$post_types = array_values( get_post_types( $args, 'names') );
+	$post_type = esc_sql( $post_types );
+	$post_type_in_string = "'" . implode( "','", $post_type ) . "'";
+	$sql = $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_title = lower(%s) AND post_type IN ($post_type_in_string)", strtolower( get_search_query() ) );
+	
+	$post = $wpdb->get_var( $sql );
+	if ( $post ) {
+		return get_post( $post );
 	}
+
+	return NULL;
 }
 
-
-function dokumento_create_new_page_from_search() {
+function dokumento_create_new_page() {
 	$output = '';
-	
-	$post_types = array_values( get_post_types( array(), 'names') );
-	$direct_match = get_page_by_title( get_search_query(), 'OBJECT', $post_types );
-	
-	if( !$direct_match ) {
-		$admin_query = 'post-new.php?post_title=' .  urlencode( ucfirst( get_query_var('s') ) );
-		$output = '<p>Create a new page called <a href="' . get_admin_url( $admin_query ) . '">' . ucfirst( get_search_query() ) . '</a>?</p>';
+	if( !is_direct_match() ) {
+		$admin_query = 'post-new.php?post_title=' . urlencode( ucfirst( get_search_query() ) );
+		$output = '<p>Create a new page called <a href="' . get_admin_url() . $admin_query . '">' . ucfirst( get_search_query() ) . '</a>?</p>';
 	}
 	
 	return $output;
+}
+
+if( ( $post = is_direct_match() ) && $wp_query->found_posts === 1 ) {
+	$redirect = get_permalink( $post->ID );
+	wp_redirect( $redirect );
+	die();
 }
 
 get_header();
@@ -28,7 +41,7 @@ get_header();
 
 <div id="content" role="main">
 <?php if (have_posts()) : ?>
-	<?php echo dokumento_create_new_page_from_search(); ?>
+	<?php echo dokumento_create_new_page(); ?>
 <?php while (have_posts()) : the_post(); ?>
 		<h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 		
@@ -39,7 +52,7 @@ get_header();
 			</div>
 	<?php endwhile; else: ?>
 		<h1 class="page-title">No results found!</h1>
-		<?php echo dokumento_create_new_page_from_search(); ?>
+		<?php echo dokumento_create_new_page(); ?>
 	<?php endif; ?>
 </div>
 <?php get_sidebar(); ?>
