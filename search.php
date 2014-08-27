e9cd2d34de55a9cb47ec6f2640f73ced1efc15fb
Russell Heimlich <kingkool68@gmail.com>
2014-08-27 15:46:03 -0400

    Fancied up the search page...
    -- Created a function for determining if we should show the "Create new page called <SEARCH TERM>?" message
    
    -- If only one search result item is found, just redirect right to that thing

--- search.php
+++ search.php
@@ -1,8 +1,34 @@
-<?php get_header(); ?>
+<?php
+global $wp_query;
+if( $wp_query->found_posts === 1 ) {
+	$post = $wp_query->posts[0];
+	$redirect_to = get_permalink( $post->ID );
+	if( get_permalink( $post->ID ) ) {
+		wp_redirect( $redirect_to );
+	}
+}
+
+
+function dokumento_create_new_page_from_search() {
+	$output = '';
+	
+	$post_types = array_values( get_post_types( array(), 'names') );
+	$direct_match = get_page_by_title( get_search_query(), 'OBJECT', $post_types );
+	
+	if( !$direct_match ) {
+		$admin_query = 'post-new.php?post_title=' .  urlencode( ucfirst( get_query_var('s') ) );
+		$output = '<p>Create a new page called <a href="' . get_admin_url( $admin_query ) . '">' . ucfirst( get_search_query() ) . '</a>?</p>';
+	}
+	
+	return $output;
+}
+
+get_header();
+?>
 
 <div id="content" role="main">
 <?php if (have_posts()) : ?>
-	<p>Create a new page called <a href="<?php echo get_admin_url()?>post-new.php?post_title=<?php echo urlencode( ucfirst( get_query_var('s') ) );?>"><?php echo ucfirst( get_search_query() ); ?></a>?</p>
+	<?php echo dokumento_create_new_page_from_search(); ?>
 <?php while (have_posts()) : the_post(); ?>
 		<h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 		
@@ -13,7 +39,7 @@
 			</div>
 	<?php endwhile; else: ?>
 		<h1 class="page-title">No results found!</h1>
-		<p>Create a new page called <a href="<?php echo get_admin_url()?>post-new.php?post_title=<?php echo urlencode( ucfirst( get_query_var('s') ) );?>"><?php echo ucfirst( get_search_query() ); ?></a>?</p>
+		<?php echo dokumento_create_new_page_from_search(); ?>
 	<?php endif; ?>
 </div>
 <?php get_sidebar(); ?>
